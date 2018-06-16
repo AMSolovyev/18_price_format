bb63596967037fbd0277f2be49e4e4382d50ae33
AMSolovyev <alexander.solovyev7@gmail.com>
2018-06-16 22:38:02 +0300

    I have corrected the script with pep8

--- tests.py
+++ tests.py
@@ -6,26 +6,22 @@ class TestGetFormatPrice(unittest.TestCase):
     def test_check_digits(self):
         price = 12.0000
         self.assertEqual(get_format_price(price), 12)
-        price = 12,000
+        price = 12, 000
         self.assertIsNone(get_format_price(price), 12000)
         price = 12000
         self.assertIsNotNone(get_format_price(price))
-
 
     def test_check_negative(self):
         price = - 12.00
         self.assertTrue(get_format_price(price))
 
-
     def test_check_empty(self):
         price = ''
         self.assertIsNone(get_format_price(price))
 
-
     def test_check_string(self):
         price = 'fgh356'
         self.assertIsNone(get_format_price(price))
-
 
     def test_cheks_zero(self):
         price = 0
@@ -34,4 +30,3 @@ class TestGetFormatPrice(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
